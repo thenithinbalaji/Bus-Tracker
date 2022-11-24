f12eb7d83acb7c2032ab1f9fbd8297f1081b94f1
thenithinbalaji <thenithinbalaji@gmail.com>
2022-11-25 02:53:52 +0530

    90% backend done (hopefully)

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,4 @@
+// asset for custom map marker
 image = "https://icons.iconarchive.com/icons/flaticonmaker/flat-style/48/bus-icon.png"
 
 const locations = [
@@ -5,17 +6,17 @@ const locations = [
   ["KK Nagar", 13.041, 80.1994],
   ["Egmore", 13.0732, 80.2609],
   ["Koyembedu", 13.0694, 80.1948],
-  ["SSN", 12.7517, 80.2033],
 ];
 
-var data;
-var userbusno;
+var data, userbusno, lat, long;
 
+// getting bus number of currently logged in users
 async function getuserbus(){
   const response = await fetch("/busno")
   userbusno = await response.json()
 }
 
+// get location of buses from mongodb 
 async function getlocation(){
   const response = await fetch("/location")
   data = await response.json()
@@ -24,8 +25,6 @@ async function getlocation(){
 function initMap() {
   bus_number = document.getElementById("choice-busroute").value
   location_number = document.getElementById("choice-location").value
-
-  console.log(data)
 
   var center = { lat: 13.0827, lng: 80.2707 };
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -43,23 +42,25 @@ function initMap() {
         ),
         map: map,
         icon: image,
-        title: data[count][0],
-        animation: google.maps.Animation.DROP,
+        title: "Route " + String(data[count][0]),
+        animation: google.maps.Animation.BOUNCE,
       });
     }
 
-    if(data[count][0] == 5 - location_number){
+    if(count == location_number){
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(
-          data[count][1],
-          data[count][2]
+          locations[count][1],
+          locations[count][2]
         ),
         map: map,
-        title: data[count][0],
+        title: String(locations[count][0]),
         animation: google.maps.Animation.DROP,
       });
     }
-  }
+    
+    
+}
 }
 
 window.initMap = initMap;
@@ -68,19 +69,29 @@ function forgotpwd() {
   prompt("To reset your password, submit your username or your email address below. If we can find you in the database, an email will be sent to your email address, with instructions how to get access again.", "");
 }
 
-function currentpos(){
+async function currentpos(){
   const successCallback = (position) => {
-    console.log(position);
-    console.log(position.coords);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+
+    const senddata = {busno: userbusno, lat: lat, long: long}
+
+    fetch("/sharelocation", {
+      method: "POST", 
+      body: JSON.stringify(senddata),
+      headers: {'Content-Type': 'application/json'}
+    }).then(res => {
+      console.log("Request complete! response:", res);
+    });
+
   };
 
   const errorCallback = (error) => {
     console.log(error);
   };
 
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+
   console.log(userbusno)
   alert("Your Location has been shared for Bus " + userbusno)
 
